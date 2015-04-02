c77d941f72ced6f3368becc4d5ddac6e0ce5dc13
james <zetlen@gmail.com>
2015-04-02 09:28:32 -0500

    adding transform in simulate function

--- simulator.js
+++ simulator.js
@@ -1,5 +1,30 @@
+function transformContextConfig(cfg) {
+  var newCfg = {
+    get: {},
+    exec: {}
+  };
+
+  Object.keys(cfg.get).forEach(function(key) {
+    if (typeof cfg.get[key] === "function") {
+      newCfg.get[key] = cfg.get[key];
+    } else {
+      newCfg.get[key] = function() {
+        return cfg.get[key];
+      }
+    }
+  });
+
+  Object.keys(cfg.exec).forEach(function(key) {
+    newCfg.exec[key] = cfg.exec[key];
+  });
+
+  return newCfg;
+};
+
+
+
 module.exports = function(actionName, implementation, contextConfig, callback) {
   return function() {
-    return implementation.call(null, contextConfig, callback);
+    return implementation.call(null, transformContextConfig(contextConfig), callback);
   };
 }
